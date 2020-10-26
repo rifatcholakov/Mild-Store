4a95b65950868600b443a71a7d5b39347cb09514
Rifat Cholakov <rifatcholakov@Gmail.com>
2020-10-26 21:02:02 +0200

    add redux to cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import formatCurrency from '../util';
 import Fade from 'react-reveal/Fade';
+import { removeFromCart } from '../actions/cartActions';
 
-export default class Cart extends Component {
+class Cart extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -160,3 +162,10 @@ export default class Cart extends Component {
         );
     }
 }
+
+export default connect(
+    state => ({
+        cartItems: state.cart.cartItems
+    }),
+    { removeFromCart }
+)(Cart);
