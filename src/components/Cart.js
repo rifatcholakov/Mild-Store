22079db6092a7b2488fc28312fc8351ceb9c0479
Rifat Cholakov <rifatcholakov@Gmail.com>
2020-10-29 11:42:53 +0200

    fixing a little bug

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -40,7 +40,7 @@ class Cart extends Component {
                     <div className="cart cart-header">Cart is empty</div>
                 ) : (
                     <div className="cart cart-header">
-                        You have {cartItems.length} in the cart{' '}
+                        You have {cartItems.length} items in the cart{' '}
                     </div>
                 )}
                 <div>
