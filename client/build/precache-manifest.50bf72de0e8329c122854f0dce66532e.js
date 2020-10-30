8f25a71fcd2cea6e8dd8972714df65f483435fc9
Rifat Cholakov <rifatcholakov@Gmail.com>
2020-10-30 20:43:04 +0200

    creating build

---
+++ client/build/precache-manifest.50bf72de0e8329c122854f0dce66532e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e01d114d3f43b59f2acc0e756aa7f9e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6922079ea7657583da2d",
+    "url": "/static/css/main.250a5748.chunk.css"
+  },
+  {
+    "revision": "261010a2a52cfcefa149",
+    "url": "/static/js/2.ccdaec1f.chunk.js"
+  },
+  {
+    "revision": "df0557c1fa2ab91a9f63d934edc4d1cb",
+    "url": "/static/js/2.ccdaec1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6922079ea7657583da2d",
+    "url": "/static/js/main.e8a88fa7.chunk.js"
+  },
+  {
+    "revision": "05fcb81cb8342c1973c8",
+    "url": "/static/js/runtime-main.fb77b8ba.js"
+  }
+]);
